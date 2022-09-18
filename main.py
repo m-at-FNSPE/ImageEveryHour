40244d93fa3e1804934aea575c081bc89d3b7fa1
m-at-FNSPE <matejpopda@gmail.com>
2022-09-18 22:27:06 +0200

    merged

--- main.py
+++ main.py
@@ -5,13 +5,11 @@ import urllib.request as request
 import html
 from functools import wraps
 
-
 # API keys dictionary names
 google_api_key = "google_API_key"
 google_cx_key = "google_cx_key"
 import cv2
 
-# TODO log what webs you are requesting
 
 test_image_1_url = "https://images.squarespace-cdn.com/content/v1/560c3567e4b03e0837f47270/1493669481606-9EGYMM9PVBLNNTFAZUJ9/IMG_3541.JPG?format=1500w"
 test_image_2_url = "http://static1.squarespace.com/static/597e7c7d893fc0db64795f0d/6162e23b8ad294334b6994c0/62d0bc41f71f7410e8559e0d/1657846856920/IMG_3541.JPG?format=1500w"
@@ -53,36 +51,44 @@ def log(x):
                     f.write("\n" + datetime.datetime.now().strftime("%m/%d/%Y, %H:%M") + "\n")
                 f.write("Being written to from " + func.__name__ + " function\n")
                 f.write(x + "\n")
-                f.write(str(result) + "\n")
+                f.write("Calling the function with: " + str(args) + "\n")
                 log.times_called_this_run += 1
             return result
         return wrapper
     return decorator
 
 
-@log("Searching this query: ")
 def get_url_for_google_search(query: str, offset: int = 0):
     url_start = "https://customsearch.googleapis.com/customsearch/v1?"
     key = "key=" + str(api_keys()[google_api_key])
     cx = "cx=" + str(api_keys()[google_cx_key])
     q = "q=" + query
-    start = "offset=" + str(offset)
+    start = "start=" + str(offset)
 
     return url_start + key + "&" + cx + "&" + q + "&" + start
 
 
-def urls_of_images_from_query(query: str, offset: int = 0):
-    # print(get_url_for_google_search(query))
+@log("Searching this query: ")
+def urls_of_images_from_google_cx(query: str):
     result = []
-    response = request.urlopen(get_url_for_google_search(query, offset)).read()
+    response = request.urlopen(query).read()
     x = json.loads(response)["items"]
     for item in x:
-        result.append(item["pagemap"]["cse_image"][0]["src"])
+        try:  # sometimes the query doesn't contain the correct path
+            result.append(item["pagemap"]["cse_image"][0]["src"])
+        except KeyError:
+            pass
     return result
 
 
+def get_url_of_images(query, offset):
+    return urls_of_images_from_google_cx(get_url_for_google_search(query, offset))
+
+
+@log("Retrieving image from")
 def download_an_image(url):  # TODO might need to handle different file types? but should be fine to keep as is
     request.urlretrieve(url, temp_file_name)
+
 
 def parse_query(word: str):
     result = ""
@@ -102,17 +108,19 @@ def download_face_detection_cascade():
     # bellow is not a permalink, might stop working (is probably fine though)
     url = "https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml"
     request.urlretrieve(url, face_detection_cascade_filename)
+
+
 def random_word_from_queries_list():
     # We assume that the list is short enough
     with open("QueriesList", "r") as f:
         x = f.read()
     x = x.splitlines()
-    return x[random.randint(0, len(x) - 1)]
+    return parse_query(x[random.randint(0, len(x) - 1)])
 
 
-def how_many_faces_in_image(image):  # TODO modify this to detect other things (animals for example) too
+def how_many_faces_in_image(image_path):  # TODO modify this to detect other things (animals for example) too
     face_cascade = cv2.CascadeClassifier(face_detection_cascade_filename)
-    image = cv2.imread(image)
+    image = cv2.imread(image_path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(
         gray,
@@ -123,12 +131,28 @@ def how_many_faces_in_image(image):  # TODO modify this to detect other things (
     )
     return len(faces)
 
-def random_query():
-    return parse_query(random_word_from_queries_list())
+
+def get_images_with_faces_from_query(query: str, offset: int = 0):
+    q = []
+    while len(q) == 0:
+        images = get_url_of_images(query, offset)
+        for i in images:
+            download_an_image(i)
+            if how_many_faces_in_image(temp_file_name) > 0:
+                q.append(i)
+        offset += 10
+    return q
+
+
+
 
 
 if __name__ == '__main__':
-    download_an_image(test_image_1_url)
-    print(how_many_faces_in_image(temp_file_name))
+    download_face_detection_cascade()
+    results = get_images_with_faces_from_query(random_word_from_queries_list())
+    for face in results:
+        print(face)
+
+
 
 
